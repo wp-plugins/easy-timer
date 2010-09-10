6b5f1967722786e12b25d879eb7e7f105055b954
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-10 01:09:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easy-timer/tags/1.1@287617 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/hu.js
@@ -0,0 +1,34 @@
+var string0second = '0 m&aacute;sodperc';
+var string1day = '1 nap';
+var string1hour = '1 &oacute;ra';
+var string1minute = '1 perc';
+var string1second = '1 m&aacute;sodperc';
+var stringdays = 'nap';
+var stringhours = '&oacute;ra';
+var stringminutes = 'perc';
+var stringseconds = 'm&aacute;sodperc';
+
+var stringmonth = new Array(13);
+stringmonth[0] = 'DECEMBER';
+stringmonth[1] = 'JANU&aacute;R';
+stringmonth[2] = 'FEBRU&aacute;R';
+stringmonth[3] = 'M&aacute;RCIUS';
+stringmonth[4] = 'ÁPRILIS';
+stringmonth[5] = 'M&aacute;JUS';
+stringmonth[6] = 'J&uacute;NIUS';
+stringmonth[7] = 'J&uacute;LIUS';
+stringmonth[8] = 'AUGUSZTUS';
+stringmonth[9] = 'SZEPTEMBER';
+stringmonth[10] = 'OKT&oacute;BER';
+stringmonth[11] = 'NOVEMBER';
+stringmonth[12] = 'DECEMBER';
+
+var stringweekday = new Array(8);
+stringweekday[0] = 'VAS&aacute;RNAP';
+stringweekday[1] = 'H&eacute;TF&otilde;';
+stringweekday[2] = 'KEDD';
+stringweekday[3] = 'SZERDA';
+stringweekday[4] = 'CS&uuml;T&ouml;RT&ouml;K';
+stringweekday[5] = 'P&eacute;NTEK';
+stringweekday[6] = 'SZOMBAT';
+stringweekday[7] = 'VAS&aacute;RNAP';
