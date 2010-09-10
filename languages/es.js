6e357da9c633fd41268d4427af9aa63af0422da7
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-10 01:05:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easy-timer/tags/1.6@287611 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/es.js
@@ -0,0 +1,34 @@
+var string0second = '0 segundo';
+var string1day = '1 d&iacute;a';
+var string1hour = '1 hora';
+var string1minute = '1 minuto';
+var string1second = '1 segundo';
+var stringdays = 'd&iacute;as';
+var stringhours = 'horas';
+var stringminutes = 'minutos';
+var stringseconds = 'segundos';
+
+var stringmonth = new Array(13);
+stringmonth[0] = 'DICIEMBRE';
+stringmonth[1] = 'ENERO';
+stringmonth[2] = 'FEBRERO';
+stringmonth[3] = 'MARZO';
+stringmonth[4] = 'ABRIL';
+stringmonth[5] = 'MAYO';
+stringmonth[6] = 'JUNIO';
+stringmonth[7] = 'JULIO';
+stringmonth[8] = 'AGOSTO';
+stringmonth[9] = 'SEPTIEMBRE';
+stringmonth[10] = 'OCTUBRE';
+stringmonth[11] = 'NOVIEMBRE';
+stringmonth[12] = 'DICIEMBRE';
+
+var stringweekday = new Array(8);
+stringweekday[0] = 'DOMINGO';
+stringweekday[1] = 'LUNES';
+stringweekday[2] = 'MARTES';
+stringweekday[3] = 'MI&Eacute;RCOLES';
+stringweekday[4] = 'JUEVES';
+stringweekday[5] = 'VIERNES';
+stringweekday[6] = 'S&Aacute;BADO';
+stringweekday[7] = 'DOMINGO';
