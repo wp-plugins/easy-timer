9f3dc3e70d4c4ba72ee7ff1d0d2e4f4d21ba85f7
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-17 07:56:36 +0000

    * Version 3.5 	* Add a meta box in the edit pages of posts and pages
    git-svn-id: https://plugins.svn.wordpress.org/easy-timer/tags/3.5@773857 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/meta-box/fr.php
@@ -0,0 +1,5 @@
+<?php $links = array(
+'' => 'Documentation',
+'#countdown-timers' => 'Comment afficher un compteur de temps restant&nbsp;?',
+'#date' => 'Comment afficher l\'heure ou la date&nbsp;?',
+'#screen-options-wrap' => 'Cacher cette boîte');
