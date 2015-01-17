b836ff7809c293cfa4b4c15f96cf195f6ee18617
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-17 19:13:44 +0000

    * Version 3.6.6 	* Add Bulgarian translation
    git-svn-id: https://plugins.svn.wordpress.org/easy-timer/tags/3.6.6@1070097 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/lang-js.php
@@ -0,0 +1,37 @@
+<script type="text/javascript">
+string0day = '';
+string0hour = '';
+string0minute = '';
+string0second = ' <?php _e('0 second', 'easy-timer'); ?>';
+string1day = ' <?php _e('1 day', 'easy-timer'); ?>';
+string1hour = ' <?php _e('1 hour', 'easy-timer'); ?>';
+string1minute = ' <?php _e('1 minute', 'easy-timer'); ?>';
+string1second = ' <?php _e('1 second', 'easy-timer'); ?>';
+stringNdays = ' [N] <?php _e('days', 'easy-timer'); ?>';
+stringNhours = ' [N] <?php _e('hours', 'easy-timer'); ?>';
+stringNminutes = ' [N] <?php _e('minutes', 'easy-timer'); ?>';
+stringNseconds = ' [N] <?php _e('seconds', 'easy-timer'); ?>';
+stringmonth = <?php echo json_encode(array(
+__('DECEMBER', 'easy-timer'),
+__('JANUARY', 'easy-timer'),
+__('FEBRUARY', 'easy-timer'),
+__('MARCH', 'easy-timer'),
+__('APRIL', 'easy-timer'),
+__('MAY', 'easy-timer'),
+__('JUNE', 'easy-timer'),
+__('JULY', 'easy-timer'),
+__('AUGUST', 'easy-timer'),
+__('SEPTEMBER', 'easy-timer'),
+__('OCTOBER', 'easy-timer'),
+__('NOVEMBER', 'easy-timer'),
+__('DECEMBER', 'easy-timer'))); ?>;
+stringweekday = <?php echo json_encode(array(
+__('SUNDAY', 'easy-timer'),
+__('MONDAY', 'easy-timer'),
+__('TUESDAY', 'easy-timer'),
+__('WEDNESDAY', 'easy-timer'),
+__('THURSDAY', 'easy-timer'),
+__('FRIDAY', 'easy-timer'),
+__('SATURDAY', 'easy-timer'),
+__('SUNDAY', 'easy-timer'))); ?>;
+</script>
